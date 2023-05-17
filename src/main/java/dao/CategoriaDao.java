68c5293e130fb260e5f9ecacf4e29dab07e4d0a0
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2023-05-17 15:40:12 -0600

    implements crud methods

--- src/main/java/dao/CategoriaDao.java
+++ src/main/java/dao/CategoriaDao.java
@@ -1,21 +1,112 @@
 package dao;
 
 import models.Categoria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.hibernate.service.UnknownServiceException;
+import utils.HibernateUtil;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.List;
 
 public class CategoriaDao implements Crud<Categoria> {
+
+    private SessionFactory sessionFactory;
+    private Session session;
+
+    private Transaction transaction;
+
+    public CategoriaDao () {
+
+    }
+
     @Override
-    public boolean insert () {
-        return false;
+    public boolean insert (Categoria entity) {
+
+        try {
+            sessionFactory = HibernateUtil.getSessionFactory ();
+
+            session = sessionFactory.openSession ();
+
+            transaction = session.beginTransaction ();
+
+            session.persist (entity);
+
+            transaction.commit ();
+
+            session.close ();
+            HibernateUtil.closeSessionFactory ();
+
+        } catch (UnknownServiceException ex) {
+            Logger.getLogger (CategoriaDao.class.getName ()).log (Level.SEVERE, ex.getMessage(), ex);
+        }
+        return true;
     }
 
     @Override
     public Categoria getById (long id) {
-        return null;
+
+        Categoria category = new Categoria ();
+        List<Categoria> categoryList;
+
+        try {
+            sessionFactory = HibernateUtil.getSessionFactory ();
+
+            session = sessionFactory.openSession ();
+
+            transaction = session.beginTransaction ();
+
+            Query<Categoria> query = session.createNativeQuery ("select * from categorias where categoriaid=:qid", Categoria.class);
+            query.setParameter ("qid", id);
+
+            categoryList = query.list ();
+
+            // no category with specified id available
+            if (categoryList.isEmpty ()) {
+                return null;
+            }
+
+            category = categoryList.get (0);
+
+
+        } catch (UnknownServiceException ex) {
+            Logger.getLogger (CategoriaDao.class.getName ()).log (Level.SEVERE, ex.getMessage(), ex);
+        } finally {
+            session.close ();
+            HibernateUtil.closeSessionFactory ();
+        }
+        return category;
     }
 
     @Override
     public Categoria get (Categoria entity) {
         return null;
+    }
+
+    @Override
+    public List<Categoria> getAll () {
+        List<Categoria> categoryList = null;
+        try {
+            sessionFactory = HibernateUtil.getSessionFactory ();
+
+            session = sessionFactory.openSession ();
+
+            transaction = session.beginTransaction ();
+
+            Query<Categoria> query = session.createQuery ("from models.Categoria", Categoria.class);
+
+            categoryList = query.list ();
+
+        } catch (UnknownServiceException ex) {
+            Logger.getLogger (CategoriaDao.class.getName ()).log (Level.SEVERE, ex.getMessage(), ex);
+        } finally {
+            session.close ();
+            HibernateUtil.closeSessionFactory ();
+        }
+
+        return categoryList;
     }
 
     @Override
