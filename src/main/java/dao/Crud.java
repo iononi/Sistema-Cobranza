48dfc260ca15f27d70bb04bf61a40546ff53cead
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2023-05-17 15:41:57 -0600

    add method to get all records

--- src/main/java/dao/Crud.java
+++ src/main/java/dao/Crud.java
@@ -1,9 +1,14 @@
 package dao;
 
+import java.util.List;
+
 public interface Crud<T> {
-    boolean insert (); // create
+    boolean insert (T entity); // create
     T getById (long id); // read
     T get (T entity); // read
+
+    List<T> getAll (); // read
+
     boolean update (T entity); // update
     boolean delete (T entity); // delete
 }
