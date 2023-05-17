0637fbb48324f3dde6a3ec976c60e0b2a9c0003b
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2023-05-17 15:35:57 -0600

    add postgres dialect

---
+++ src/main/java/utils/HibernateUtil.java
@@ -0,0 +1,35 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+
+    private static StandardServiceRegistry buildServiceRegistryBuilder () {
+        final StandardServiceRegistry registry;
+        registry = new StandardServiceRegistryBuilder()
+                .configure ()
+                .build ();
+
+        return registry;
+    }
+
+    private static SessionFactory buildSessionFactory (StandardServiceRegistry registry) {
+        return new MetadataSources(registry)
+                .buildMetadata ()
+                .buildSessionFactory ();
+    }
+
+    // Singleton design pattern to ensure only one SessionFactory object
+    public static SessionFactory getSessionFactory () {
+        if (sessionFactory == null) {
+            // apply xml configuration and start the sessionFactory
+            try (StandardServiceRegistry registry = buildServiceRegistryBuilder ()) {
+                sessionFactory = buildSessionFactory (registry);
+            }
+        }
+
+        return sessionFactory;
+    }
+}
